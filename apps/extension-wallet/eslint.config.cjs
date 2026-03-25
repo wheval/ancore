3804098dcad13f2c8f42bf66bf3c9fd4807ad2af
David-patrick-chuks <pd3072894@gmail.com>
2026-03-25 04:11:59 +0100

    fix(extension-wallet): repair flat eslint config

--- apps/extension-wallet/eslint.config.cjs
+++ apps/extension-wallet/eslint.config.cjs
@@ -11,20 +11,12 @@ module.exports = [
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       parser: tsparser,
-      globals: {
-        ...globals.browser,
-        ...globals.vitest,
-      },
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
       },
       globals: {
         ...globals.browser,
@@ -33,27 +25,26 @@ module.exports = [
     },
     plugins: {
       '@typescript-eslint': tseslint,
-      'react': react,
+      react,
       'react-hooks': reactHooks,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off', // Not needed in React 18+
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-undef': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
     },
     settings: {
       react: {
         version: 'detect',
       },
-    },
-    },
-    rules: {
-      ...tseslint.configs.recommended.rules,
-      'no-undef': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     },
   },
   {
@@ -61,6 +52,7 @@ module.exports = [
     languageOptions: {
       globals: {
         ...globals.jest,
+        ...globals.vitest,
         vi: 'readonly',
       },
     },
